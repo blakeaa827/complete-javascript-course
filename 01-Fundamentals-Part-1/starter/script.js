d93a7deda7a50fbae4fcd218a8f118a5907bf1b3
blakeaa827 <78KxDwgit config --global user.name blakeaa827W2%q>
2022-02-15 14:12:43 -0600

    Finished coding challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -70,3 +70,54 @@ if (bmiMark > bmiJohn) {
 }
 */
 
+/*
+Coding Challenge #3
+
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+
+Test data:
+  Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+  Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+  Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+/*
+// const scoreDolphins = [96, 108, 89];
+// const scoreKoalas = [88, 91, 110];
+
+// const scoreDolphins = [97, 112, 101];
+// const scoreKoalas = [109, 95, 123];
+
+const scoreDolphins = [97, 112, 101];
+const scoreKoalas = [109, 95, 106];
+
+let calcAvg = function (scores) {
+    const denominator = scores.length;
+    let total = 0;
+    while (scores.length) total += scores.shift();
+    return total / denominator;
+}
+
+const avgDolphins = calcAvg(scoreDolphins);
+const avgKoalas = calcAvg(scoreKoalas);
+
+if ((avgDolphins + avgKoalas) / 2 < 100) console.log(`Neither team scored the minimum average of 100 points.`)
+else if (avgDolphins > avgKoalas) console.log(`Dolphins win with an average score of ${avgDolphins.toPrecision(4)} to ${avgKoalas.toPrecision(4)}.`)
+else if (avgDolphins < avgKoalas) console.log(`Koalas win with an average score of ${avgKoalas.toPrecision(4)} to ${avgDolphins.toPrecision(4)}.`)
+else console.log(`It's a tie of ${avgKoalas.toPrecision(4)}`);
+*/
+
